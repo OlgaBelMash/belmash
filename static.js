ac44af0a0c298f83b64164f53cb387a0d2379119
OlgaBelMash <43036782+OlgaBelMash@users.noreply.github.com>
2018-09-07 10:03:37 +0200

    Update static.js

--- static.js
+++ static.js
@@ -1,6 +1,4 @@
 $("#footer").append("<div class=\"links flex-center\">Все права защищены СООО Завод Белмаш 2018 ©</div>")
-
-
 var $topSliderChecker = setInterval(
     function(){
         var $slick_header_top = $('.slider-header');
@@ -17,4 +15,20 @@ var $topSliderChecker = setInterval(
 		}
     },
 100);
-
+$(window).ready(function () {
+	if(!localStorage.getItem('cookieAccepted')){
+		var cookieHTML="<div id='cookieNotification' style='{background: #002c7e;width: 100%;height: 30px;position: fixed;bottom: 0px;z-index: 9999;color: #fee;padding-left: 15px;}'>"+
+		  "<p style='display: inline-block;'>Сайт belmash.by использует файлы cookies и сервисы сбора технических данных посетителей (данные об IP-адресе, местоположении и др.) для более удобной и быстрой работы."+
+		  "Продолжая посещение нашего сайта, вы автоматически соглашаетесь на использование cookies."+
+		  "<a href='/informacija/cookie/nasha-politika-faylov-cookie/' style='line-height: 30px;color: #c81717;'>Подробнее...</a></p>"+
+		  "<button id='cookieButton' style='background: none;border: none;float: right;	margin-right: 15px;'>"+
+		  "	<span style='font-size: 25px;line-height: 30px;'>×</span>"+
+		  "</button>"+
+		"</div>"
+		$("#footer").append(cookieHTML);
+	}
+}
+$(document).on('click','#cookieButton', function(){
+	localStorage.setItem('cookieAccepted', 1);
+	$('#cookieNotification').remove();
+});
