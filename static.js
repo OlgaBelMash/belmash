dbec0c826e3a15e6dcf28c06b64502412526700c
OlgaBelMash <43036782+OlgaBelMash@users.noreply.github.com>
2018-09-07 10:27:12 +0200

    Update static.js

--- static.js
+++ static.js
@@ -18,7 +18,7 @@ var $topSliderChecker = setInterval(
 $(window).ready(function () {
 	if(!localStorage.getItem('cookieAccepted')){
 		var cookieHTML="<div id='cookieNotification' style='font-size: 13px;background: #002c7e;width: 100%;height: 30px;position: fixed;bottom: 0px;z-index: 9999;color: #fee;padding-left: 15px;'>"+
-		  "<p style='display: inline-block;'>Сайт belmash.by использует файлы cookies и сервисы сбора технических данных посетителей (данные об IP-адресе, местоположении и др.) для более удобной и быстрой работы."+
+		  "<p style='display: inline-block;'>Сайт belmash.by использует файлы cookies и сервисы сбора технических данных посетителей (данные об IP-адресе, местоположении и др.) для более удобной и быстрой работы. "+
 		  "Продолжая посещение нашего сайта, вы автоматически соглашаетесь на использование cookies."+
 		  "<a href='/informacija/cookie/nasha-politika-faylov-cookie/' target='_blank' style='line-height: 30px;color: #c81717;'>&nbsp;Подробнее...</a></p>"+
 		  "<button id='cookieButton' style='background: none;border: none;float: right;	margin-right: 15px;'>"+
