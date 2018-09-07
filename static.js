6d12ebc97834c253ed9d8e14c8d97a43775fc6fd
OlgaBelMash <43036782+OlgaBelMash@users.noreply.github.com>
2018-09-07 07:41:50 +0200

    Update static.js

--- static.js
+++ static.js
@@ -1,10 +1,20 @@
-$("#footer").append("<div class=\"links flex-center\">Все права защищены СООО \"Завод Белмаш\" 2018 ©</div>")
-var $slick_header_top = $('.slider-header');
-$slick_header_top.unslick();
-$slick_header_top.slick({
-    arrows: false,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    infinite: true
-});
+$("#footer").append("<div class=\"links flex-center\">Все права защищены СООО Завод Белмаш 2018 ©</div>")
+
+
+var $topSliderChecker = setInterval(
+    function(){
+        var $slick_header_top = $('.slider-header');
+        if($slick_header_top.unslick){
+            $slick_header_top.unslick();
+            $slick_header_top.slick({
+                arrows: false,
+                dots: true,
+                autoplay: true,
+                autoplaySpeed: 5000,
+                infinite: true
+            });
+			clearInterval ($topSliderChecker);
+		}
+    },
+5000);
+
