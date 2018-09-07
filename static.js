fcf3d31463b388f11f3a484df7c3a6a03afcfbc2
OlgaBelMash <43036782+OlgaBelMash@users.noreply.github.com>
2018-09-07 07:22:04 +0200

    Update static.js

--- static.js
+++ static.js
@@ -1,9 +1,9 @@
 $("#footer").append("<div class=\"links flex-center\">Все права защищены СООО \"Белмаш\" 2018 ©</div>")
-var $slick_header = $('.slider-header');
+/*var $slick_header = $('.slider-header');
 $slick_header.slick({
     arrows: false,
     dots: true,
     autoplay: true,
     autoplaySpeed: 5000,
     infinite: true
-});
+});*/
