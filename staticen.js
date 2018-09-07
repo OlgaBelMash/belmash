27aa2bf18d9fdb8de37fcdf4d16391e9bd22c6f0
OlgaBelMash <43036782+OlgaBelMash@users.noreply.github.com>
2018-09-07 15:32:57 +0300

    Update staticen.js

--- staticen.js
+++ staticen.js
@@ -20,7 +20,7 @@ $(window).ready(function () {
 		var cookieHTML="<div id='cookieNotification' style='font-size: 13px;background: #002c7e;width: 100%;height: 30px;position: fixed;bottom: 0px;z-index: 9999;color: #fee;padding-left: 15px;'>"+
 		  "<p style='display: inline-block;'>Website belmash.by uses cookies to ensure that we give you the best user experiences. "+
 		  "By using this site you agree to the use of cookies for analytics and personalized content."+
-		  "<a href='/informacija/cookie/nasha-politika-faylov-cookie/' target='_blank' style='line-height: 30px;color: #c81717;'>&nbsp;More...</a></p>"+
+		  "<a href='/en/news/cookie/policy-of-cookie-files/' target='_blank' style='line-height: 30px;color: #c81717;'>&nbsp;More...</a></p>"+
 		  "<button id='cookieButton' style='background: none;border: none;float: right;	margin-right: 15px;'>"+
 		  "	<span style='font-size: 25px;line-height: 30px;'>×</span>"+
 		  "</button>"+
