f4468a9001c219139c91ac6cb7dd4a8659a26c95
OlgaBelMash <43036782+OlgaBelMash@users.noreply.github.com>
2018-10-04 13:18:41 +0200

    Update staticen.js

--- staticen.js
+++ staticen.js
@@ -18,7 +18,7 @@ var $topSliderChecker = setInterval(
 $(window).ready(function () {
 	if(!localStorage.getItem('cookieAccepted')){
 		var cookieHTML="<div id='cookieNotification' style='font-size: 13px;background: #002c7e;width: 100%;height: 30px;position: fixed;bottom: 0px;z-index: 9999;color: #fee;padding-left: 15px;'>"+
-		  "<div style='display: inline-block;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;max-width: 97%;padding-right: 25px;>'Website belmash.by uses cookies to ensure that we give you the best user experiences. "+
+		  "<div style='display: inline-block;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;max-width: 97%;padding-right: 25px;'>'Website belmash.by uses cookies to ensure that we give you the best user experiences. "+
 		  "By using this site you agree to the use of cookies for analytics and personalized content."+
 		  "<a href='/en/news/cookie/policy-of-cookie-files/' target='_blank' style='line-height: 30px;color: #c81717;'>&nbsp;More...</a></div>"+
 		  "<button id='cookieButton' style='background: none;border: none;float: right;	right: 10px;position: absolute;'>"+
