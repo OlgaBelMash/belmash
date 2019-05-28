9280cbd065357449a038109ce46d8599d3879819
OlgaBelMash <43036782+OlgaBelMash@users.noreply.github.com>
2019-05-28 10:40:16 +0200

    Update static.js

--- static.js
+++ static.js
@@ -65,7 +65,7 @@ $(document).on('click','#yw0 .item', function(){
 		$hoverChecker=false;
 	}
 });
-$(".social-links").append("<a target="_blank" href="https://www.linkedin.com/company/belmash/">
+$(".social-links").append('<a target="_blank" href="https://www.linkedin.com/company/belmash/">
 	<svg class="social-icon social-icon_linkedin" version="1.1" id="svg_linkedin" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="430.113px" height="430.114px" viewBox="0 50 512 512" xml:space="preserve">
 		<path fill="#828282" d="M150.65,100.682c0,27.992-22.508,50.683-50.273,50.683c-27.765,0-50.273-22.691-50.273-50.683
         C50.104,72.691,72.612,50,100.377,50C128.143,50,150.65,72.691,150.65,100.682z M143.294,187.333H58.277V462h85.017V187.333z
@@ -73,4 +73,4 @@ $(".social-links").append("<a target="_blank" href="https://www.linkedin.com/com
         c31.268,0,46.289,22.071,46.289,61.615c0,39.545,0,144.181,0,144.181h84.605c0,0,0-100.344,0-173.915
         s-41.689-109.131-99.934-109.131s-82.768,45.369-82.768,45.369V187.333z"/>
 	</svg>
-</a>");
+</a>');
