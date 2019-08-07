6f21a4c5037985af0e3116fdcfa97826817b5304
OlgaBelMash <43036782+OlgaBelMash@users.noreply.github.com>
2019-08-07 14:36:04 +0200

    Update staticen.js

--- staticen.js
+++ staticen.js
@@ -50,7 +50,7 @@ $(window).ready(function () {
 	$(".dropdown-catalog-tree").css("min-width","350px");
 	
 	$("#descr iframe").each(function(ifr) {
-  		$('$("#docs.col-sm-12")' ).append($(this).content());
+  		$("#docs.col-sm-12").append($(this).content());
 	});
 });
 $(document).on('click','#cookieButton', function(){
