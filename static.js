6b6f8c78a0a83db69ae4b29b3ed902cbb9fcc877
OlgaBelMash <43036782+OlgaBelMash@users.noreply.github.com>
2019-09-16 15:12:18 +0200

    Update static.js

--- static.js
+++ static.js
@@ -76,7 +76,7 @@ $(window).ready(function () {
 		$(".facebook_right").stop(true, false).animate({
 			right: "0"
 		}, 800, 'easeOutQuint');
-	}
+	});
 	$(".facebook_right").stop(true, false).animate({
 			right: "-245"
 	}, 800, 'easeInQuint');
@@ -85,7 +85,7 @@ $(window).ready(function () {
 		$(".facebook_left").stop(true, false).animate({
 			left: "0"
 		}, 800, 'easeOutQuint');
-	}
+	});
 	$(".facebook_left").stop(true, false).animate({
 			left: "-245"
 	}, 800, 'easeInQuint');
