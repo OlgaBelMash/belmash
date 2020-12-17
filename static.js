657cc0ea5ee1a30ea3b2ceeaec18251c492776c3
OlgaBelMash <43036782+OlgaBelMash@users.noreply.github.com>
2020-12-17 10:01:31 +0300

    Update static.js

--- static.js
+++ static.js
@@ -126,7 +126,7 @@ var element = $("#yw0 .items .tree-card:last-child").detach();
 $('#yw0 .items').prepend(element);
 element.css('width', '100%');
 
-var $delay=1000;
+/*var $delay=1000;
 var $hoverChecker;
 $(document).on('mouseenter','#yw0 .item', function(){
 	if(!$hoverChecker){
@@ -144,7 +144,7 @@ $(document).on('click','#yw0 .item', function(){
 		clearTimeout($hoverChecker);
 		$hoverChecker=false;
 	}
-});
+});*/
 //$(".dropdown-parent a[href$='/uslugi/']").html('<a href="/uslugi/"><span class="title">УСЛУГИ<i class="fa fa-angle-down" aria-hidden="true"></i></span></a>');
 $(".dropdown-parent a[href$='/uslugi/']>span.title").append("<i class='fa fa-angle-down' aria-hidden='true'></i>");
 $(".dropdown-parent a[href$='/uslugi/'] +ul").html('<li><a class="active" href="/uslugi/obrabotka-listovogo-material/">ЛАЗЕРНАЯ РЕЗКА</a></li><li><a href="/uslugi/tokarnaya-obrabotka-detaley/">ТОКАРНАЯ ОБРАБОТКА</a></li><li><a href="/uslugi/frezernaya-obrabotka-detaley/">ФРЕЗЕРОВАНИЕ</a></li><li><a href="/uslugi/slesarnye-raboty/">СЛЕСАРНЫЕ РАБОТЫ</a></li><li><a href="/uslugi/svarka-metallokonstrukciy/">СВАРКА МЕТАЛЛА</a></li><li><a href="/uslugi/lite-detaley-iz-razlichnyh-tipov-plastmass/">ЛИТЬЕ ПЛАСТМАСС</a></li><li><a href="/uslugi/printer-shirokoformatnoy-pechati/">ШИРОКОФОРМАТНАЯ ПЕЧАТЬ</a></li>');
