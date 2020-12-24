03c688875124d3425458554124ad2606fc30a442
OlgaBelMash <43036782+OlgaBelMash@users.noreply.github.com>
2020-12-24 11:55:56 +0300

    Update static.js

--- static.js
+++ static.js
@@ -1,7 +1,7 @@
 // snow
-/*$.getScript("//olgabelmash.github.io/belmash/snow-it.min.js", function() {
+$.getScript("//olgabelmash.github.io/belmash/snow-it.min.js", function() {
     $.fn.snowit({flakeColor: 'LightCyan',total: 100,minSize : 15,maxSize : 40, speed: $(document).height()/105});
-});*/
+});
 $("#footer").append("<div style=\"padding-bottom:35px;\" class=\"links flex-center\">Все права защищены СООО Завод Белмаш 2018-2020 ©</div>")
 var $topSliderChecker = setInterval(
     function(){
